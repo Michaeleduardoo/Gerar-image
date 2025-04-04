a567d289afe16934b5e30582112daec70e92b4ad
Michaeleduardoo <michaeledu20@outlook.com>
2025-04-04 11:28:55 -0300

    troca de nome sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./components/Style/main.scss";
+import "./components/Sass/main.scss";
 import App from "./components/App";
 
 createRoot(document.getElementById("root")!).render(
